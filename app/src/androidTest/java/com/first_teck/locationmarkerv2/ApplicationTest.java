25cf5beb2a5c01a50fae19a21680261de431573e
zhang17 <zhang17@umd.edu>
2016-01-03 01:28:48 -0500

    First commit.

---
+++ app/src/androidTest/java/com/first_teck/locationmarkerv2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.first_teck.locationmarkerv2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
